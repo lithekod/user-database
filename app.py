d3df8db359004742750ad1210068288f762b8e8d
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-01-16 12:11:09 +0100

    Add member table

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import uuid
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -5,3 +7,22 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
+class Member(db.Model):
+
+    id = db.Column(db.LargeBinary(length=16), unique=True, primary_key=True)
+    liuid = db.Column(db.String(16), nullable=False)
+    email = db.Column(db.String(64), nullable=False)
+
+
+    def __init__(self, liuid, email, **kwargs):
+        super(Member, self).__init__(**kwargs)
+
+        self.id = uuid.uuid4().bytes
+        self.liuid = liuid
+        self.email = email
+
+
+    def __repr__(self):
+        return "<id={}, liuid={}, email={}>"\
+                .format(str(uuid.UUID(bytes=self.id)), self.liuid, self.email)
