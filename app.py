633d3842228e99faa502618da658309a505a7382
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-01-16 18:50:17 +0100

    Switch to SQLite3

--- app.py
+++ app.py
@@ -1,28 +1,57 @@
 import uuid
+import sqlite3
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
+db = sqlite3.connect(':memory:')
 
-class Member(db.Model):
+def create_database():
 
-    id = db.Column(db.LargeBinary(length=16), unique=True, primary_key=True)
-    liuid = db.Column(db.String(16), nullable=False)
-    email = db.Column(db.String(64), nullable=False)
+    """Member table
+    This table contains all of the information we store on members.
+    """
+    db.execute(
+        """
+        CREATE TABLE member
+        (
+            id     BLOB     UNIQUE PRIMARY KEY,
+            liu_id TEXT     NOT NULL,
+            email  TEXT     NOT NULL,
+            active BOOLEAN  NOT NULL
+        )
+        """
+    )
 
+    """Link table
+    This table spcifies the active links which can be accessed by a member.
+    The type specifies what a link should do:
+        0 - Removes the member associated with member_id from the database.
+        1 - Sets the active status of the member associated with member_id to 1.
+    """
+    db.execute(
+        """
+        CREATE TABLE link
+        (
+            id         BLOB  UNIQUE PRIMARY KEY,
+            type       INT   NOT NULL,
+            member_id  BLOB  NOT NULL,
+            FOREIGN KEY(member_id) REFERENCES member(id)
+        )
+        """
+    )
 
-    def __init__(self, liuid, email, **kwargs):
-        super(Member, self).__init__(**kwargs)
+# Experimental
+def add_member(liuid, email):
+    db.execute("INSERT INTO member VALUES (?, ?, ?, ?)",
+        (uuid.uuid4().bytes, liuid, email, 0))
 
-        self.id = uuid.uuid4().bytes
-        self.liuid = liuid
-        self.email = email
+# Experimental
+def add_link(member_id):
+    db.execute("INSERT INTO link VALUES (?, ?, ?)",
+        (uuid.uuid4().bytes, 0, member_id))
 
+# Experimental
+def delete_member(member_id):
+    db.execute("DELETE FROM member WHERE id = ?", (member_id,))
 
-    def __repr__(self):
-        return "<id={}, liuid={}, email={}>"\
-                .format(str(uuid.UUID(bytes=self.id)), self.liuid, self.email)
