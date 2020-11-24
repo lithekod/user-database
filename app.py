e8700cbf6d3fe393df859751a56ae147ba1b302b
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-11-24 21:21:03 +0100

    Update some text shown to users when updating membership

--- app.py
+++ app.py
@@ -258,7 +258,7 @@ def handle_link(link):
 
     elif action_id == "DELETE":
         if "confirm" not in request.args:
-            message = "<a href=\"/" + url + "?confirm=true\">Confirm</a>"
+            message = "<p style='margin: 0;'>End membership? (Cannot be reversed)</p><a href=\"/" + url + "?confirm=true\">Confirm</a>"
         else:
             modify_db(DELETE_LINK_WITH_MEMBER_ID, (liuid,))
             modify_db(DELETE_MEMBER_WITH_ID, (liuid,))
