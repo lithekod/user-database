506bb9a8b1f728a16cda4b40e1a4410d0d9bc7d5
Erik Mattfolk <erik.mattfolk@gmail.com>
2021-01-08 11:51:36 +0100

    Remove config from git

--- config.py
+++
@@ -1,6 +0,0 @@
-SENDER_EMAIL = "no-reply@lithekod.se"
-SENDER_PASSWORD = "dev"
-SERVER_URL = "localhost:5000"
-DATABASE_PATH = "/tmp/test.db"
-SECRET_KEY = "dev"
-ACTIONS = ["SHOW", "RENEW", "DELETE", "UNSUBSCRIBE"]
