a63bcfb24d9f5413fbe70e90196aef5e5fdc5d90
Erik Mattfolk <erik.mattfolk@gmail.com>
2021-02-23 21:37:08 +0100

    Add example configuration file

---
+++ _config.py
@@ -0,0 +1,13 @@
+SECRET_KEY = "dev"
+GITHUB_WEBHOOK_SECRET = "dev"
+
+SENDER_EMAIL = "no-reply@lithekod.se"
+SENDER_PASSWORD = "dev"
+
+SERVER_URL = "localhost:5000"
+
+DATABASE_PATH = "/tmp/temp.db"
+ACTIONS = ["SHOW", "RENEW", "DELETE", "UNSUBSCRIBE"]
+
+STANDINGS_PATH = "aoc/aoc_standings.json"
+INVALID_CONTESTANTS = set()
