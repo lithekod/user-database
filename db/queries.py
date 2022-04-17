7f0b53c79428878225803ad65692e2bb72bf432e
Erik Mattfolk <erik.mattfolk@gmail.com>
2022-04-17 19:42:42 +0200

    Add delete inactive members query

--- db/queries.py
+++ db/queries.py
@@ -6,10 +6,12 @@ SELECT_MEMBER_ID = "SELECT id FROM member"
 SELECT_LINK_WITH_URL = "SELECT * FROM link WHERE url=?"
 SELECT_LINK_MEMBERID_LINK = "SELECT member_id, url FROM link"
 SELECT_MEMBER_WITH_ID = "SELECT * FROM member WHERE id=?"
-SELECT_MEMBER_ACTIVE = " ".join([
-    "SELECT * FROM member",
-    "WHERE strftime('%Y', renewed) = strftime('%Y', CURRENT_TIMESTAMP)",
-])
+SELECT_MEMBER_ACTIVE = " ".join(
+    [
+        "SELECT * FROM member",
+        "WHERE strftime('%Y', renewed) = strftime('%Y', CURRENT_TIMESTAMP)",
+    ]
+)
 SELECT_MEMBER_INACTIVE = SELECT_MEMBER_ACTIVE.replace("=", "!=")
 SELECT_MEMBER_SUBSCRIBED = "SELECT * FROM member WHERE subscribed=1"
 
@@ -21,3 +23,4 @@ DELETE_LINK = "DELETE FROM link"
 DELETE_LINK_WITH_IDS = "DELETE FROM link WHERE member_id=? AND action_id=?"
 DELETE_LINK_WITH_MEMBER_ID = "DELETE FROM link WHERE member_id=?"
 DELETE_MEMBER_WITH_ID = "DELETE FROM member WHERE id=?"
+DELETE_MEMBER_INACTIVE = SELECT_MEMBER_INACTIVE.replace("SELECT", "DELETE")
